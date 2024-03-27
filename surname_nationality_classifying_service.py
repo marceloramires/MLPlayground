ecc0ae0237e1c853819b578e052311699ed13ac3
Marcelo Ramires <marceloramires@Marcelos-MacBook-Air.local>
2024-03-26 18:04:20 -0700

    Enabling CORS for testing

--- surname_nationality_classifying_service.py
+++ surname_nationality_classifying_service.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from utils.ModelRunner import *
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 runner = ModelRunner()
+cors = CORS(app)
 
 @app.route("/<name>")
+@cross_origin()
 def predict(name):
     return runner.predict(name) 
